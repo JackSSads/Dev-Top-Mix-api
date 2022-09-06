4443cf0e316e9f5d4bbda036a055eafbc4c4bc33
JackSSads <fotosjackson2018@hotmail.com>
2022-09-06 02:32:49 -0300

    Feature: Importando GestorModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClienteModule } from './modules/cliente/cliente.module';
 import { ColaboradorModule } from './modules/colaborador/colaborador.module';
+import { GestorModule } from './modules/gestor/gestor.module';
 
 @Module({
-  imports: [ClienteModule, ColaboradorModule],
+  imports: [ClienteModule,
+    ColaboradorModule,
+    GestorModule],
   controllers: [],
   providers: [],
 })
